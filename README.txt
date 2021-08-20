0cedffa65edb8c0a320c2c91414bdf0cb171aab1
Qing Lyu <amy.lyu@mail.utoronto.ca>
2021-08-20 20:48:26 +0800

    updated readme and project.info and .sql

--- README.txt
+++ README.txt
@@ -2,19 +2,23 @@
 a. OS recommendation:
 Ubuntu Linux 18.04
 b. Application Server:
-Download and install Django 3.2.4
+Download and install Django 2.1.5
 c. Database Server:
 Download and install MySQL 5.7
 Note: table names are not case-sensitive.
+d. Connection Pool
+Download and install DBUtils 1.3
 
 2.Database initialization
 a. Create a database named ***, e.g. training.
-b. Under MySQL client, enter into the *** to run db/ou.sql 
+b. Under MySQL client, enter into the *** to run db/sys_setup.sql 
 
 3. Application Deployment
-a. Copy src to Django installation directory, if src has already exist, delete it first.
+a. Copy src to Django installation directory. If src has already exist, delete it first.
 b. Modify Connection Pool configuration:
-Inside src/trydjango/settings.py, from lines 88-103, change DATABASES by replacing NAME with your MySQL database name ***, USER with your MySQL account username, PASSWORD with your MySQL database password. Keep everything else unchanged.
+    Inside src/trydjango/settings.py, from lines 88-103, change DATABASES by replacing NAME with your MySQL database name ***, USER with your MySQL account username, PASSWORD with your MySQL database password. Keep everything else unchanged.
+    Inside src/training/db.py, from lines 7-24, change POOL by replacing database with your MySQL database name ***, user with your MySQL account username, password with your MySQL database password. Keep everything else unchanged.
+
 
 4. Run Django
 
--- db/sys_setup.sql
+++ db/sys_setup.sql
@@ -4,36 +4,36 @@ CREATE TABLE ou_account(
     username VARCHAR(32) NOT NULL   COMMENT '' ,
     passwd VARCHAR(32) NOT NULL   COMMENT '' ,
     email VARCHAR(32) NOT NULL   COMMENT '' ,
-    status VARCHAR(1) NOT NULL   COMMENT 'A/Active,I/Inavtive,F/Forbidden' ,
+    userstatus VARCHAR(1) NOT NULL   COMMENT 'A/Active,I/Inavtive,F/Forbidden' ,
     deptcode VARCHAR(32) NOT NULL   COMMENT '' ,
-    registertime VARCHAR(19) NOT NULL   COMMENT '' ,
+    registeredtime VARCHAR(19) NOT NULL   COMMENT '' ,
     lastlogin VARCHAR(19)    DEFAULT  NULL COMMENT '' ,
     lastloginip VARCHAR(32)    DEFAULT  NULL COMMENT '' ,
     seqnum INT    DEFAULT  NULL COMMENT '' ,
     PRIMARY KEY (usercode)
-)  COMMENT = 'Account';
+)  COMMENT = '账号表';
 
 CREATE TABLE ou_dept(
     id INT NOT NULL   COMMENT '' ,
     deptcode VARCHAR(32) NOT NULL   COMMENT '' ,
     pathcode VARCHAR(256) NOT NULL   COMMENT 'xxx/yyy/zzz' ,
     deptname VARCHAR(64) NOT NULL   COMMENT '' ,
-    status VARCHAR(1) NOT NULL   COMMENT 'A/Active,I/Inactive' ,
+    deptstatus VARCHAR(1) NOT NULL   COMMENT 'A/Active,I/Inactive' ,
     description VARCHAR(64)    DEFAULT  NULL COMMENT '' ,
     seqnum INT    DEFAULT  NULL COMMENT '' ,
     PRIMARY KEY (deptcode)
-)  COMMENT = 'Department';
+)  COMMENT = '部门表';
 
 CREATE TABLE ou_resource(
     id INT NOT NULL   COMMENT '' ,
-    code VARCHAR(128) NOT NULL   COMMENT '' ,
-    name VARCHAR(32) NOT NULL   COMMENT '' ,
+    resourcecode VARCHAR(128) NOT NULL   COMMENT '' ,
+    resourcename VARCHAR(32) NOT NULL   COMMENT '' ,
     sysname VARCHAR(32) NOT NULL   COMMENT '' ,
     modelname VARCHAR(32) NOT NULL   COMMENT '' ,
     actionname VARCHAR(32) NOT NULL   COMMENT '' ,
     accesstype VARCHAR(1) NOT NULL   COMMENT 'L/login access,A/anonymous,R/role' ,
-    PRIMARY KEY (code)
-)  COMMENT = 'Resource';
+    PRIMARY KEY (resourcecode)
+)  COMMENT = '资源表';
 
 CREATE TABLE ou_role(
     id INT NOT NULL   COMMENT '' ,
@@ -42,27 +42,42 @@ CREATE TABLE ou_role(
     description VARCHAR(64)    DEFAULT  NULL COMMENT '' ,
     seqnum INT    DEFAULT  NULL COMMENT '' ,
     PRIMARY KEY (rolecode)
-)  COMMENT = 'Role';
+)  COMMENT = '角色表';
 
 CREATE TABLE ou_roleresource(
     id INT NOT NULL   COMMENT '' ,
-    rolecode VARCHAR(32)    COMMENT '' ,
+    rolecode VARCHAR(32) NOT NULL   COMMENT '' ,
     resourcecode VARCHAR(128) NOT NULL   COMMENT '' ,
     rightflag VARCHAR(1) NOT NULL   COMMENT 'Y/Yes,N/No' ,
-    PRIMARY KEY (rolecode)
-)  COMMENT = 'Roleresource';
+    PRIMARY KEY (rolecode,resourcecode,rightflag)
+)  COMMENT = '角色资源表';
 
 CREATE TABLE ou_userrole(
     id INT NOT NULL   COMMENT '' ,
     usercode VARCHAR(32) NOT NULL   COMMENT '' ,
     rolecode VARCHAR(32) NOT NULL   COMMENT '' ,
     PRIMARY KEY (usercode)
-)  COMMENT = 'Userrole';
+)  COMMENT = '用户角色表';
 
-CREATE TABLE gl_dic(
+CREATE TABLE gl_dictionary(
     id INT NOT NULL   COMMENT '' ,
+    typename VARCHAR(32) NOT NULL   COMMENT '' ,
     dickey VARCHAR(64) NOT NULL   COMMENT '' ,
     dicvalue VARCHAR(64) NOT NULL   COMMENT '' ,
-    PRIMARY KEY (dickey)
-)  COMMENT = 'Systemdict';
+    PRIMARY KEY (typename,dickey)
+)  COMMENT = '系统字典表';
+
+CREATE TABLE salaries_salary(
+    id INT NOT NULL AUTO_INCREMENT  COMMENT '' ,
+    employeeid INT NOT NULL   COMMENT '' ,
+    name VARCHAR(64) NOT NULL   COMMENT '' ,
+    age INT NOT NULL   COMMENT '' ,
+    department VARCHAR(64) NOT NULL   COMMENT '' ,
+    hiredate VARCHAR(19) NOT NULL   COMMENT '' ,
+    salary INT NOT NULL   COMMENT '' ,
+    level INT NOT NULL   COMMENT '' ,
+    subsidy Decimal(10)    DEFAULT  NULL COMMENT '' ,
+    total Decimal(10)    DEFAULT  NULL COMMENT '' ,
+    PRIMARY KEY (id)
+)  COMMENT = '人员薪水表';
 
